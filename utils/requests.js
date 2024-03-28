9cfd8afdfc5d781b1188f7f1394220abca24cc77
Sefrion <ilyenkov96@gmail.com>
2024-03-28 11:44:20 +0200

    fetch single property api route and fetching from client

--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// Fetch all properties
 export async function fetchProperties() {
 	try {
 		// Handle case when domain is not yet available
@@ -14,5 +15,23 @@ export async function fetchProperties() {
 	} catch (error) {
 		console.log(error);
 		return [];
+	}
+}
+
+// Fetch single property
+export async function fetchProperty(id) {
+	try {
+		// Handle case when domain is not yet available
+		if (!apiDomain) {
+			return null;
+		}
+		const res = await fetch(`${apiDomain}/properties/${id}`);
+		if (!res.ok) {
+			throw new Error('Failed to fetch data');
+		}
+		return res.json();
+	} catch (error) {
+		console.log(error);
+		return null;
 	}
 }
