73ff8de1fb1ac9229c5c2f2cebf15a525632b035
Sefrion <ilyenkov96@gmail.com>
2024-04-19 11:17:54 +0300

    sihn ou and middleware added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -188,6 +188,7 @@ const Navbar = () => {
 											role='menuitem'
 											tabIndex='-1'
 											id='user-menu-item-0'
+											onClick={() => setIsProfileMenuOpen(false)}
 										>
 											Your Profile
 										</Link>
@@ -197,10 +198,15 @@ const Navbar = () => {
 											role='menuitem'
 											tabIndex='-1'
 											id='user-menu-item-2'
+											onClick={() => setIsProfileMenuOpen(false)}
 										>
 											Saved Properties
 										</Link>
 										<button
+											onClick={() => {
+												setIsProfileMenuOpen(false);
+												signOut();
+											}}
 											className='block px-4 py-2 text-sm text-gray-700'
 											role='menuitem'
 											tabIndex='-1'
