127f4b7d582814647c39004e19506175981b2e49
Sefrion <ilyenkov96@gmail.com>
2024-04-20 12:33:31 +0300

    /api/properties POST endpoint added

--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -86,7 +86,7 @@ const PropertyAddForm = () => {
 	};
 
 	return (
-		<form>
+		<form action='/api/properties' method='POST' encType='multipart/form-data'>
 			<h2 className='text-3xl text-center font-semibold mb-6'>Add Property</h2>
 
 			<div className='mb-4'>
@@ -470,7 +470,7 @@ const PropertyAddForm = () => {
 				<input
 					type='text'
 					id='seller_name'
-					name='seller_info.name.'
+					name='seller_info.name'
 					className='border rounded w-full py-2 px-3'
 					placeholder='Name'
 					value={fields.seller_info.name}
@@ -524,6 +524,7 @@ const PropertyAddForm = () => {
 					className='border rounded w-full py-2 px-3'
 					accept='image/*'
 					multiple
+					required
 					onChange={handleImageChange}
 				/>
 			</div>
