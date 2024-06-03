b0d92eac68064a60fabc8b449bcac734a7b46c74
Sefrion <ilyenkov96@gmail.com>
2024-06-03 12:08:23 +0300

    cloadinary added to upload images

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,6 +1,7 @@
 import connectDB from '@/config/db';
 import Property from '@/models/Property';
 import { getSessionUser } from '@/utils/getSessionUser';
+import cloudinary from '@/config/cloudinary';
 
 // GET /api/properties
 export const GET = async (request) => {
@@ -54,8 +55,36 @@ export const POST = async (req) => {
 				phone: formData.get('seller_info.phone')
 			},
 			owner: userId
-			// images
 		};
+
+		// Upload image(s) to Cloudinary
+		const imageUploadPromises = [];
+
+		for (const image of images) {
+			const imageBuffer = await image.arrayBuffer();
+			const imageArray = Array.from(new Uint8Array(imageBuffer));
+			const imageData = Buffer.from(imageArray);
+
+			// Convert the image data to base64
+			const imageBase64 = imageData.toString('base64');
+
+			// Make request to upload to Cloudinary
+			const result = await cloudinary.uploader.upload(
+				`data:image/png;base64,${imageBase64}`,
+				{
+					folder: 'propertypulse'
+				}
+			);
+
+			imageUploadPromises.push(result.secure_url);
+
+			// Wait for all images to upload
+			const uploadedImages = await Promise.all(imageUploadPromises);
+
+			// Add uploaded images to the propertyData object
+			propertyData.images = uploadedImages;
+		}
+
 		const newProperty = new Property(propertyData);
 		await newProperty.save();
 		return Response.redirect(
