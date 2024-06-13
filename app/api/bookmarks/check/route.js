62a3785eced2f965dbd3cf11e4aa992d45c5da43
Sefrion <ilyenkov96@gmail.com>
2024-06-13 08:24:53 +0300

    added ability ot bookmark properties

---
+++ app/api/bookmarks/check/route.js
@@ -0,0 +1,35 @@
+import connectDB from '@/config/db';
+import User from '@/models/User';
+import { getSessionUser } from '@/utils/getSessionUser';
+
+export const dynamic = 'force-dynamic';
+
+export const POST = async (req) => {
+	try {
+		await connectDB();
+
+		const { propertyId } = await req.json();
+
+		const sessionUser = await getSessionUser();
+
+		if (!sessionUser || !sessionUser.userId) {
+			return new Response('User ID is required', { status: 401 });
+		}
+
+		const { userId } = sessionUser;
+
+		// Find user in database
+		const user = await User.findById(userId);
+
+		// Check if property was bookmarked
+		let isBookmarked = user.bookmarks.includes(propertyId);
+
+		return new Response(JSON.stringify({ isBookmarked }), {
+			status: 200
+		});
+	} catch (error) {
+		console.log(error);
+
+		return new Response('Something went wrong', { status: 500 });
+	}
+};
