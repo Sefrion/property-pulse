a6b0e36dc712948801e777db5d5f9c995abe85d0
Sefrion <ilyenkov96@gmail.com>
2024-06-21 10:12:56 +0300

    added api routes to display, mark as read and delete messages as well as messages page and components

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -4,6 +4,42 @@ import { getSessionUser } from '@/utils/getSessionUser';
 
 export const dynamic = 'force-dynamic';
 
+// GET /api/messages
+export const GET = async () => {
+	try {
+		await connectDB();
+
+		const sessionUser = await getSessionUser();
+
+		if (!sessionUser || !sessionUser.user) {
+			return new Response(JSON.stringify({ msg: 'User ID is required' }), {
+				status: 401
+			});
+		}
+
+		const { userId } = sessionUser;
+
+		const readMessages = await Message.find({ recipient: userId, read: true })
+			.sort({ createdAt: -1 })
+			.populate('sender', 'username')
+			.populate('property', 'name');
+
+		const unreadMessages = await Message.find({ recipient: userId, read: false })
+			.sort({ createdAt: -1 })
+			.populate('sender', 'username')
+			.populate('property', 'name');
+
+		const messages = [...unreadMessages, ...readMessages];
+
+		return new Response(JSON.stringify(messages), { status: 200 });
+	} catch (error) {
+		console.log(error);
+		return new Response(JSON.stringify({ msg: 'Something went wrong' }), {
+			status: 200
+		});
+	}
+};
+
 // POST /api/messages
 export const POST = async (request) => {
 	try {
